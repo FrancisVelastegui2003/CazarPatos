9ef8d07576415ca02094a6cf6dfe13fc539b193a
P3E009-E <P3E009-E@39681629D20P3>
2025-07-18 10:30:11 -0500

    Added functionality to call Firebase Firestore on MainActivity and RankingActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.security.crypto)
+    implementation(libs.firebase.firestore)
+    implementation(libs.google.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/francis/velastegui/cazarpatos/RankingActivity.kt
+++ app/src/main/java/com/francis/velastegui/cazarpatos/RankingActivity.kt
@@ -1,11 +1,16 @@
 package com.francis.velastegui.cazarpatos
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.francis.velastegui.cazarpatos.database.RankingPlayerDBHelper
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class RankingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +36,10 @@ class RankingActivity : AppCompatActivity() {
         recyclerViewRanking.setHasFixedSize(true);
         */
 
-        OperacionesSqLite()
-        GrabarRankingSQLite()
-        LeerRankingsSQLite()
+        //OperacionesSqLite()
+        //GrabarRankingSQLite()
+        //LeerRankingsSQLite()
+        consultarPuntajeJugadores()
     }
 
     fun OperacionesSqLite(){
@@ -64,5 +70,32 @@ class RankingActivity : AppCompatActivity() {
         recyclerViewRanking.adapter = RankingAdapter(jugadoresSQLite)
         recyclerViewRanking.setHasFixedSize(true)
     }
+
+    fun consultarPuntajeJugadores() {
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .orderBy("huntedDucks", Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener { result ->
+                Log.d(EXTRA_LOGIN, "Success getting documents")
+                var jugadores = ArrayList<Player>()
+                for (document in result) {
+                    val jugador = document.toObject(Player::class.java)
+                    //val jugador = document.toObject<Player>()
+                    jugadores.add(jugador)
+                }
+                //Poblar en RecyclerView información usando mi adaptador
+                val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+                recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+                recyclerViewRanking.adapter = RankingAdapter(jugadores);
+                recyclerViewRanking.setHasFixedSize(true);
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error getting documents.", exception)
+                Toast.makeText(this, "Error al obtener datos de jugadores", Toast.LENGTH_LONG)
+                    .show()
+            }
+    }
+
 
 }
